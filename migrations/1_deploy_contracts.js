f945552c6675dd490976c5a15a13b08158f89a7e
李名河 <2219845592@qq.com>
2023-09-22 12:49:03 +0800

    deploy

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,18 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+// const ConvertLib = artifacts.require("ConvertLib");
+// const MetaCoin = artifacts.require("MetaCoin");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+const Voting = artifacts.require("Voting");
+
+const proposalNames = [
+  "0x4100000000000000000000000000000000000000000000000000000000000000",
+  "0x4200000000000000000000000000000000000000000000000000000000000000",
+  "0x4300000000000000000000000000000000000000000000000000000000000000",
+];
+
+module.exports = function (deployer) {
+  // deployer.deploy(ConvertLib);
+  // deployer.link(ConvertLib, MetaCoin);
+  // deployer.deploy(MetaCoin);
+
+  deployer.deploy(Voting, proposalNames);
 };
